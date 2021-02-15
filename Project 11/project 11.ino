042fbb83ce58272ecd9e8058c5a00bf5e4553c35
Tanzim-Al Islam Dibosh <tanzim.1@outlook.com>
2021-02-16 00:14:38 +0530

    Update project 11.ino

--- Project 11/project 11.ino
+++ Project 11/project 11.ino
@@ -1,16 +1,16 @@
 void setup()
 {
-Serial.begin(9600);
-pinMode(7,INPUT);// input in digital 7
+  Serial.begin(9600);
+  pinMode(7,INPUT);// input in digital 7
 // analog pin A0 declaration not required
 }
 void loop()
 {
-Serial.print("Analog Reading=");
-int analog_val = analogRead(0);
-Serial.print(analog_val);
-Serial.print("\t Digital Reading=");
-int digital_val = digitalRead(7);
-Serial.println(digital_val);
-delay(1000);
+  Serial.print("Analog Reading=");
+  int analog_val = analogRead(0);
+  Serial.print(analog_val);
+  Serial.print("\t Digital Reading=");
+  int digital_val = digitalRead(7);
+  Serial.println(digital_val);
+  delay(1000);
 }
