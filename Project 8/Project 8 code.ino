19c3e1d53062fa73a40e06f09a57e33ba8ae78be
Tanzim-Al Islam Dibosh <tanzim.1@outlook.com>
2021-02-23 19:42:54 +0600

    Minor problem fixed

--- Project 8/Project 8 code.ino
+++
@@ -1,8 +0,0 @@
-void setup(){
-Serial.begin(9600); // declare serial communication
-}
-void loop(){
-int value = analogRead(0); //Read analog value from pot
-Serial.println(value);
-delay(100); //Small delay
-}
